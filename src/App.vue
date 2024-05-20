9f20e50f972631532fcf2a22174c74fd922aa53c
TeyKz <81241850+TeyKz@users.noreply.github.com>
2024-05-20 23:36:10 +0200

    chore: Update file paths in App.vue and Navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import Navbar from './components/partials/Navbar.vue'
 import Home from '../src/HomePage/Home.vue'
 import AboutMe from '../src/HomePage/AboutMe.vue'
